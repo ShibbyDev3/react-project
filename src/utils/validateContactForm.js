054f5d9978bd239f29bc3f349bf557b9445acded
Eddy Gomez <contact@eddyg.dev>
2023-03-05 17:47:48 -0500

    contact page

---
+++ src/utils/validateContactForm.js
@@ -0,0 +1,21 @@
+export const validateContactForm = (values) => {
+    const errors = {};
+     
+    if(!values.email) {
+        errors.email = 'Required';
+    }
+
+    if(!values.firstName) {
+        errors.firstName = 'Required';
+    }
+
+    if(!values.lastName) {
+        errors.lastName = 'Required';
+    }
+
+    if(!values.request) {
+        errors.request = 'Required';
+    }
+
+    return errors
+}
